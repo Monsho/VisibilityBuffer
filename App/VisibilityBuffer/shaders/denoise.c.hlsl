9d421c77db12c93886093a738444b688b8f4946f
Monsho <cinderella.cage@gmail.com>
2023-05-05 18:14:51 +0900

    shader追加忘れ

---
+++ App/VisibilityBuffer/shaders/denoise.c.hlsl
@@ -0,0 +1,66 @@
+#include "cbuffer.hlsli"
+#include "math.hlsli"
+
+ConstantBuffer<SceneCB>		cbScene		: register(b0);
+ConstantBuffer<AmbOccCB>	cbAO		: register(b1);
+
+Texture2D<float>			texDepth	: register(t0);
+Texture2D<float>			texAO		: register(t1);
+Texture2D<float>			texPrevDepth: register(t2);
+Texture2D<float>			texPrevAO	: register(t3);
+
+SamplerState				samLinearClamp	: register(s0);
+
+RWTexture2D<float4>			rwOutput	: register(u0);
+
+float ClipDepthToViewDepth(float D, float4x4 mtxViewToClip)
+{
+	return (D * mtxViewToClip[3][3] - mtxViewToClip[2][3]) / (mtxViewToClip[2][2] - D * mtxViewToClip[3][2]);
+}
+
+[numthreads(8, 8, 1)]
+void main(uint3 did : SV_DispatchThreadID)
+{
+	uint2 pixPos = did.xy;
+	float2 uv = (float2(pixPos) + 0.5) * cbScene.invScreenSize;
+
+	// current depth.
+	float depth = texDepth[pixPos];
+	float4 clipPos = float4(uv * float2(2, -2) + float2(-1, 1), depth, 1);
+	float VD = ClipDepthToViewDepth(depth, cbScene.mtxViewToProj);
+
+	// spatio denoise.
+	float radiusInPixels = cbAO.denoiseRadius * cbAO.ndcPixelSize / -VD;
+	int R = min(max(ceil(radiusInPixels), 1), 4);
+	float totalWeight = 1.0f;
+	float ao = texAO[pixPos];
+	for (int x = -R; x <= R; x++)
+	{
+		for (int y = -R; y <= R; y++)
+		{
+			if (x != 0 || y != 0)
+			{
+				int2 p = pixPos + int2(x, y);
+				float nd = texDepth[p];
+				float nVD = ClipDepthToViewDepth(nd, cbScene.mtxViewToProj);
+				float w = 1.0 - smoothstep(1.0, 2.0, abs(VD - nVD));
+				ao += texAO[p] * w;
+				totalWeight += w;
+			}
+		}
+	}
+	ao = ao / totalWeight;
+
+	// depth weight.
+	float4 prevClipPos = mul(cbScene.mtxProjToPrevProj, clipPos);
+	prevClipPos.xyz *= (1 / prevClipPos.w);
+	float2 prevUV = prevClipPos.xy * float2(0.5, -0.5) + 0.5;
+	float prevDepth = texPrevDepth.SampleLevel(samLinearClamp, prevUV, 0);
+	float prevVD = ClipDepthToViewDepth(prevDepth, cbScene.mtxPrevViewToProj);
+	float w = smoothstep(cbAO.denoiseDepthMin, cbAO.denoiseDepthMax, abs(VD - prevVD));
+	float depthWeight = 1 - w * w;
+
+	// temporal denoise.
+	float weight = cbAO.denoiseBaseWeight * depthWeight;
+	rwOutput[pixPos] = lerp(ao, texPrevAO[pixPos], weight);
+}
